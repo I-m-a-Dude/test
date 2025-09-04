2b085eb1afd9709a4acc9e7046f49ca28eef18c6
I-m-a-Dude <tudor.i.farcas1@gmail.com>
2025-09-04 19:35:07 +0300

    testing

--- Backend/tests/test_model_wrapper.py
+++ Backend/tests/test_model_wrapper.py
@@ -2,29 +2,23 @@ from unittest import TestCase
 
 
 class TestMedNeXtWrapper(TestCase):
-    def test__setup_device(self):
-        self.fail()
 
-    def test__create_model(self):
-        self.fail()
 
-    def test_force_gpu_cleanup(self):
-        self.fail()
+def test_get_model_wrapper():
+    assert False
 
-    def test_load_model(self):
-        self.fail()
 
-    def test_predict(self):
-        self.fail()
+def test_ensure_model_loaded():
+    assert False
 
-    def test_get_model_info(self):
-        self.fail()
 
-    def test_unload_model(self):
-        self.fail()
+def test_unload_global_model():
+    assert False
 
-    def test_get_memory_usage(self):
-        self.fail()
 
-    def test_force_cleanup(self):
-        self.fail()
+def test_force_global_cleanup():
+    assert False
+
+
+def test_get_global_memory_usage():
+    assert False
